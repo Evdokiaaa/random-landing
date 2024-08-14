1073703898ab2134899a1337a4c8df4317b9b823
Vladislav <vladalt1231@gmail.com.com>
2024-08-14 22:28:49 +0900

    Сделал логику в stages

--- app/js/main.js
+++ app/js/main.js
@@ -27,3 +27,52 @@ stageRightBtn.addEventListener("click", () => {
   reset();
   activateStage(currentState);
 });
+const members = document.querySelectorAll(".members__main-card");
+const memberRightBtn = document.querySelector(".members__right-btn");
+const memberLeftBtn = document.querySelector(".members__left-btn");
+const membersCurrentCount = document.querySelector(".members__current"); //Счетчик слайдов
+
+let currentMember = 0;
+const totalMembers = members.length;
+let slidesToShow = window.innerWidth <= 1200 ? 1 : 3;
+
+function updateSlide() {
+  members.forEach((member) => (member.style.display = "none"));
+
+  for (let i = currentMember; i < currentMember + slidesToShow; i++) {
+    if (i < totalMembers) {
+      members[i].style.display = "block";
+    }
+  }
+}
+function updateCounter() {
+  membersCurrentCount.textContent = currentMember + 1;
+}
+memberRightBtn.addEventListener("click", () => {
+  if (currentMember + slidesToShow < totalMembers) {
+    currentMember += slidesToShow;
+  } else {
+    currentMember = 0; // Возврат к первому
+  }
+  updateCounter();
+  updateSlide();
+});
+memberLeftBtn.addEventListener("click", () => {
+  if (currentMember - slidesToShow >= 0) {
+    currentMember -= slidesToShow;
+  } else {
+    currentMember = Math.max(totalMembers - slidesToShow, 0); // Переход к последним
+  }
+  updateCounter();
+  updateSlide();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateSlide();
+  updateCounter();
+});
+window.addEventListener("resize", () => {
+  slidesToShow = window.innerWidth <= 1200 ? 1 : 3;
+  counter = 0;
+  updateSlide();
+});
